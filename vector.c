dc3c7cc72ec7066c3e76f4b53ec2206ee9c5f597
imperosol <56346771+imperosol@users.noreply.github.com>
2021-10-13 17:56:39 +0200

    Add files via upload

--- vector.c
+++ vector.c
@@ -1,37 +1,80 @@
 #include "vector.h"
 
 
-
 static void extend_vector(Vector vect) {
     const unsigned int to_add = vect->mem_size / 2;
-    vect->mem_size += (to_add > 0 ? to_add : 1);
+    vect->mem_size += (to_add > vect->elem_size ? to_add : vect->elem_size);
     vect->array = realloc(vect->array, vect->mem_size);
 }
 
 Vector new_vector(const unsigned int elem_size, const unsigned int initial_capacity) {
     internVect *new = malloc(sizeof(internVect));
     if (new == NULL) {
-        fprintf(stderr, "Fatal: failed to allocate %zu bytes.\n", n);
+        fprintf(stderr, "Error: failed to allocate %zu bytes.\n", sizeof(internVect));
         return NULL;
     }
     new->elem_size = elem_size;
     new->array_size = 0;
-    new->mem_size = (initial_capacity > 0 ? initial_capacity : 1) * sizeof(elem_size);
+    new->mem_size = (initial_capacity > 0 ? initial_capacity : 1) * elem_size;
     new->array = safe_malloc(new->mem_size);
     return new;
 }
+
+Vector new_vector_from_array(const void* restrict array, const unsigned int elem_size, const unsigned int elem_count) {
+    internVect *new = malloc(sizeof(internVect));
+    if (new == NULL) {
+        fprintf(stderr, "Error: failed to allocate %zu bytes.\n", sizeof(internVect));
+        return NULL;
+    }
+    new->elem_size = elem_size;
+    new->array_size = elem_count;
+    new->mem_size = elem_count * elem_size;
+    new->array = malloc(new->mem_size);
+    memcpy(new->array, array, new->mem_size);
+    return new;
+}
+
+Vector new_vector_from_vector(Vector toCopy) {
+    internVect *new = malloc(sizeof(internVect));
+    if (new == NULL) {
+        fprintf(stderr, "Error: failed to allocate %zu bytes.\n", sizeof(internVect));
+        return NULL;
+    }
+    new->elem_size = toCopy->elem_size;
+    new->array_size = toCopy->array_size;
+    new->mem_size = toCopy->mem_size;
+    new->array = malloc(new->mem_size);
+    memcpy(new->array, toCopy->array, new->mem_size);
+    return new;
+}
+
 
 void drop_vector(Vector vector) {
     free(vector->array);
     free(vector);
 }
 
+void clear_vector(Vector vector) {
+    vector->array_size = 0;
+    vector->mem_size = 0;
+    free(vector->array);
+    vector->array = NULL;
+}
+
 void vector_push(Vector restrict vector, void *restrict new_elem) {
-    if (vector->array_size * vector->elem_size >= vector->mem_size) {
+    while ((vector->array_size + 1) * vector->elem_size > vector->mem_size) {
         extend_vector(vector);
     }
     memcpy(vector->array + vector->array_size * vector->elem_size, new_elem, vector->elem_size);
     ++vector->array_size;
+}
+
+void vector_push_array(Vector restrict vector, const void *restrict array, const unsigned int array_size) {
+    while ((vector->array_size + array_size) * vector->elem_size > vector->mem_size) {
+        extend_vector(vector);
+    }
+    memcpy(vector->array + vector->array_size * vector->elem_size, array, vector->elem_size * array_size);
+    vector->array_size += array_size;
 }
 
 void *vector_pop(Vector vector) {
@@ -44,7 +87,7 @@ void *vector_pop(Vector vector) {
 }
 
 void vector_insert(Vector vector, void * new_elem, const unsigned int index) {
-    if (index >= vector->array_size || vector < 0) {
+    if (index >= vector->array_size) {
         fprintf(stderr, "Error : index array out of range");
         return;
     }
@@ -58,7 +101,7 @@ void vector_insert(Vector vector, void * new_elem, const unsigned int index) {
 }
 
 void * vector_remove(Vector vector, const unsigned int index) {
-    if (index >= vector->array_size || vector < 0) {
+    if (index >= vector->array_size) {
         fprintf(stderr, "Error : index array out of range");
         return NULL;
     }
@@ -70,18 +113,43 @@ void * vector_remove(Vector vector, const unsigned int index) {
 }
 
 void *vector_get(Vector vector, const unsigned int index) {
-    if (index >= vector->array_size || vector < 0) {
+    if (index >= vector->array_size) {
         fprintf(stderr, "Error : index array out of range");
         return NULL;
     }
     return vector->array + index * vector->elem_size;
 }
 
+void vector_set(Vector vector, const unsigned int index, const void * new_elem) {
+    if (index >= vector->array_size) {
+        fprintf(stderr, "Error : list index out of range");
+        return;
+    }
+    memcpy(vector->array + index * vector->elem_size, new_elem, vector->elem_size);
+}
+
+
 unsigned int vector_get_size(Vector vector) {
     return vector->array_size;
 }
 
 void vector_trim(Vector vector) {
-    const unsigned int new_size = vector->elem_size * vector->array_size;
-    vector->array = realloc(vector->array, new_size);
+    vector->mem_size = vector->elem_size * vector->array_size;
+    vector->array = realloc(vector->array, vector->mem_size);
 }
+
+void vector_append(Vector restrict dest, Vector restrict source) {
+    if (dest->elem_size != source->elem_size) {
+        fprintf(stderr, "Error : cannot concatenate vectors of different v_type");
+        exit(E_INCOMPATIBLE_TYPES);
+    }
+    if (dest->elem_size * (dest->array_size + source->array_size) > dest->mem_size) {
+        dest->mem_size = dest->elem_size * (dest->array_size + source->array_size);
+        dest->array = realloc(dest->array, dest->mem_size);
+    }
+    memcpy(dest->array + dest->array_size * dest->elem_size,
+           source->array,
+           source->array_size * source->elem_size);
+    dest->array_size += source->array_size;
+}
+
--- vector.h
+++ vector.h
@@ -7,16 +7,28 @@
 
 #include "nyanpasu_lib.h"
 
+#define E_INCOMPATIBLE_TYPES 1
+
+#define v_elem(type, elem_ptr) *((type *) elem_ptr)
+#define v_size(vector) vector_get_size(vector)
+#define v_get(vector, index) vector_get(vector, index)
+#define v_set(vector, index, new_elem) vector_set(vector, index, new_elem)
+#define v_type(type) sizeof(type)
+
+// array_size * elem_size != mem_size
 typedef struct {
     void *array;
     unsigned int array_size;
     unsigned int elem_size;
     unsigned int mem_size;
 } internVect;
-
 typedef internVect *Vector;
 
 Vector new_vector(unsigned int elem_size, unsigned int initial_capacity);
+
+Vector new_vector_from_array(const void* restrict array, unsigned int elem_size, unsigned int elem_count);
+
+Vector new_vector_from_vector(Vector toCopy);
 
 void vector_push(Vector vector, void *new_elem);
 
@@ -24,7 +36,13 @@ void *vector_pop(Vector vector);
 
 void drop_vector(Vector vector);
 
+void clear_vector(Vector vector);
+
+void vector_trim(Vector vector);
+
 void *vector_get(Vector vector, unsigned int index);
+
+void vector_set(Vector vector, unsigned int index, const void * new_elem);
 
 void vector_insert(Vector vector, void *new_elem, unsigned int index);
 
@@ -32,5 +50,9 @@ void *vector_remove(Vector vector, unsigned int index);
 
 unsigned int vector_get_size(Vector vector);
 
+void vector_push_array(Vector restrict vector, const void *restrict array, unsigned int array_size);
 
+void vector_append(Vector restrict dest, Vector restrict source);
+
+void vector_append_self(Vector restrict dest, Vector restrict source);
 #endif //TP4_VECTOR_H
